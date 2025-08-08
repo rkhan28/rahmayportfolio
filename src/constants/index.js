8bef4a7f317e731b4ee78d1404f67f071fcb13e1
rkhan28 <rahmatkhan2001@hotmail.com>
2025-08-07 21:45:18 -0400

    Initial

---
+++ src/constants/index.js
@@ -0,0 +1,234 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  yorku,
+  tesla,
+  shopify,
+  jobit,
+  tripguide,
+  threejs,
+  carrent,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "AI Aristocrat",
+    icon: web,
+  },
+  {
+    title: "Full-Stack Web Developer",
+    icon: mobile,
+  },
+  {
+    title: "Machine Learning Engineer",
+    icon: backend,
+  },
+  {
+    title: "Deep Learning Practitioner",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+
+];
+
+const experiences = [
+  {
+    title: "Capstone Project Lead",
+    company_name: "York University",
+    icon: yorku,
+    iconBg: "#383E56",
+    date: "May 2023 - Aug 2023",
+    points: [
+      "Led a 5-member engineering team in building a sales analytics dashboard that tracked store-level inventory, revenue, and customer behavior using SQL, Python, and Power BI",
+      "Designed and automated forecasting models that broke down sales trends and inventory flow by location, improving scenario planning and territory coverage optimization",
+      "Presented executive-style reports and visual insights to professors and stakeholders, demonstrating actionable sales intelligence and strong data storytelling skills.",
+    ],
+  },
+  {
+    title: "Part-time Research Assistant",
+    company_name: "York University",
+    icon: yorku,
+    iconBg: "#383E56",
+    date: "Jan 2024 - April 2024",
+    points: [
+      "Implemented PyTorch HRNet pose‑estimation pipeline on multi‑camera sports dataset; improved PCK@0.2 by 12 %",
+      "Automated camera‑calibration and homography validation using Python, OpenCV, and NumPy; reduced preprocessing effort 30 %.",
+      "Conducted hyperparameter sweeps with TensorBoard and Weights & Biases, reporting reproducible experiments to principal investigator.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "AI Powered Financial Dashboard",
+    description:
+      "AI powered financial dashboard that provides real-time insights and analytics for better decision making.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
